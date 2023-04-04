fb12ceace8b4fb8875a0a48758c72c2564b023ae
jordan gilliam <gilliamja.te@gmail.com>
2023-04-04 15:58:41 -0400

    fix: build

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // experimental: {
-  //   fontLoaders: [
-  //     {
-  //       loader: "@next/font/google",
-  //       options: { subsets: ["latin"] },
-  //     },
-  //   ],
-  // },
+  experimental: {
+    fontLoaders: [
+      {
+        loader: "@next/font/google",
+        options: { subsets: ["latin"] },
+      },
+    ],
+  },
 }
 
 export default nextConfig
